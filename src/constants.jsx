dc6ce3a603f51b1ccab48f5ea52cafb4bb565871
JohnDanner <john@danners.org>
2021-12-18 01:00:37 +0000

    Initial commit

---
+++ src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x032E0a7dD2CFB9Fd3F4FB4F30434753EE667BAF8';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
